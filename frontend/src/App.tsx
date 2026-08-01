aa601379b411bb926f259dd875881ae2ff1fe9ec
MADufour <47703233+bleeband@users.noreply.github.com>
2026-07-31 21:42:45 -0400

    feat(frontend): brancher la route calendrier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import EditEvent from './pages/EditEvent';
 import Exports from './pages/Exports';
 import InspectionsPage from './pages/InspectionsPage';
 import InspectionDetail from './pages/InspectionDetail';
+import InspectionCalendar from './pages/InspectionCalendar';
 import ProtectedRoute from './components/common/ProtectedRoute';
 
 const queryClient = new QueryClient({
@@ -34,36 +35,13 @@ const App: React.FC = () => {
             <Route path="/login" element={<Login />} />
             <Route element={<Layout />}>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/events" element={
-                <ProtectedRoute requiredRole="VIEWER">
-                  <EventsPage />
-                </ProtectedRoute>
-              } />
-              <Route path="/events/new" element={
-                <ProtectedRoute requiredRole="MUNICIPAL_AGENT">
-                  <CreateEvent />
-                </ProtectedRoute>
-              } />
-              <Route path="/events/:id/edit" element={
-                <ProtectedRoute requiredRole="MUNICIPAL_AGENT">
-                  <EditEvent />
-                </ProtectedRoute>
-              } />
-              <Route path="/exports" element={
-                <ProtectedRoute requiredRole="MUNICIPAL_AGENT">
-                  <Exports />
-                </ProtectedRoute>
-              } />
-              <Route path="/inspections" element={
-                <ProtectedRoute requiredRole="INSPECTOR">
-                  <InspectionsPage />
-                </ProtectedRoute>
-              } />
-              <Route path="/inspections/:id" element={
-                <ProtectedRoute requiredRole="INSPECTOR">
-                  <InspectionDetail />
-                </ProtectedRoute>
-              } />
+              <Route path="/events" element={<ProtectedRoute requiredRole="VIEWER"><EventsPage /></ProtectedRoute>} />
+              <Route path="/events/new" element={<ProtectedRoute requiredRole="MUNICIPAL_AGENT"><CreateEvent /></ProtectedRoute>} />
+              <Route path="/events/:id/edit" element={<ProtectedRoute requiredRole="MUNICIPAL_AGENT"><EditEvent /></ProtectedRoute>} />
+              <Route path="/exports" element={<ProtectedRoute requiredRole="MUNICIPAL_AGENT"><Exports /></ProtectedRoute>} />
+              <Route path="/inspections" element={<ProtectedRoute requiredRole="INSPECTOR"><InspectionsPage /></ProtectedRoute>} />
+              <Route path="/inspections/calendar" element={<ProtectedRoute requiredRole="INSPECTOR"><InspectionCalendar /></ProtectedRoute>} />
+              <Route path="/inspections/:id" element={<ProtectedRoute requiredRole="INSPECTOR"><InspectionDetail /></ProtectedRoute>} />
             </Route>
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
