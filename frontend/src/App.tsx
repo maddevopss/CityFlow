209489cfcd038a34e99f1643f8fb081a0357f1e6
MADufour <47703233+bleeband@users.noreply.github.com>
2026-08-01 14:10:10 -0400

    feat(frontend): exposer la console municipale

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,7 @@ import InspectionCalendar from './pages/InspectionCalendar';
 import InspectionDashboard from './pages/InspectionDashboard';
 import InspectionTrends from './pages/InspectionTrends';
 import CitizenPortalPage from './pages/CitizenPortalPage';
+import MunicipalCitizenRequestsPage from './pages/MunicipalCitizenRequestsPage';
 import NotificationsPage from './pages/NotificationsPage';
 import ProtectedRoute from './components/common/ProtectedRoute';
 
@@ -41,6 +42,7 @@ const App: React.FC = () => (
             <Route path="/inspections/calendar" element={<ProtectedRoute requiredRole="INSPECTOR"><InspectionCalendar /></ProtectedRoute>} />
             <Route path="/inspections/:id" element={<ProtectedRoute requiredRole="INSPECTOR"><InspectionDetail /></ProtectedRoute>} />
             <Route path="/citizen" element={<ProtectedRoute requiredRole="VIEWER"><CitizenPortalPage /></ProtectedRoute>} />
+            <Route path="/municipal/citizen-requests" element={<ProtectedRoute requiredRole="MUNICIPAL_AGENT"><MunicipalCitizenRequestsPage /></ProtectedRoute>} />
             <Route path="/notifications" element={<ProtectedRoute requiredRole="VIEWER"><NotificationsPage /></ProtectedRoute>} />
           </Route>
           <Route path="*" element={<Navigate to="/" />} />
