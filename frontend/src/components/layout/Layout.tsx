21f9186bb30ecfb6dd20104fd658387dbc01ac93
MADufour <47703233+bleeband@users.noreply.github.com>
2026-08-01 21:59:11 -0400

    feat(frontend): brancher le retour en haut au contenu principal

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ScrollToTopButton } from '../common/ScrollToTopButton';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
+const MAIN_SCROLL_CONTAINER_ID = 'cityflow-main-content';
+
 const Layout: React.FC = () => {
   return (
-    <div className="flex flex-col h-screen">
+    <div className="flex h-screen flex-col">
       <Header />
       <div className="flex flex-1 overflow-hidden">
         <Sidebar />
-        <main className="flex-1 overflow-auto bg-gray-50">
+        <main
+          id={MAIN_SCROLL_CONTAINER_ID}
+          className="flex-1 overflow-auto bg-gray-50"
+          tabIndex={-1}
+        >
           <Outlet />
         </main>
+        <ScrollToTopButton scrollContainerId={MAIN_SCROLL_CONTAINER_ID} />
       </div>
     </div>
   );
