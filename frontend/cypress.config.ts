27740cf6aee72710833e6040155df9487022f76b
MADufour <47703233+bleeband@users.noreply.github.com>
2026-08-03 15:12:29 -0400

    fix(ci): déclarer l’absence de support Cypress

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:4173",
+    supportFile: false,
     video: false,
     retries: { runMode: 2, openMode: 0 },
   },
